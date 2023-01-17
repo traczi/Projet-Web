155a8a7eea14db9cd963c5cc85718227e15a5816
traczi <matteo.alcantarini@hotmail.com>
2023-01-17 17:39:54 +0100

    fix de bug pour la fonctionnalité moto

--- src/main/java/heh/be/projet_tri/Configuration.java
+++ src/main/java/heh/be/projet_tri/Configuration.java
@@ -1,11 +1,12 @@
 package heh.be.projet_tri;
 
-import heh.be.projet_tri.adaptater.out.CarMapper;
-import heh.be.projet_tri.adaptater.out.CarPersistenceAdaptater;
-import heh.be.projet_tri.adaptater.out.CarRepository;
+import heh.be.projet_tri.adaptater.out.*;
 import heh.be.projet_tri.domain.port.in.CarPortIn;
+import heh.be.projet_tri.domain.port.in.MotoPortIn;
 import heh.be.projet_tri.domain.port.out.CarPortOut;
+import heh.be.projet_tri.domain.port.out.MotoPortOut;
 import heh.be.projet_tri.domain.service.CarListUseCase;
+import heh.be.projet_tri.domain.service.MotoListUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -15,18 +16,34 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class Configuration {
     @Autowired
     private CarRepository carRepository;
+    @Autowired
+    private MotoRepository motoRepository;
 
     private CarMapper carMapper = new CarMapper();
     private CarPersistenceAdaptater carPersistenceAdaptater;
+    private MotoMapper motoMapper = new MotoMapper();
+    private MotoPersistenceAdaptater motoPersistenceAdaptater;
 
     @Bean
-    public CarPortOut getCarPortOut(){
-        return new CarPersistenceAdaptater(carRepository,carMapper);
+    public CarPortOut getCarPortOut() {
+        return new CarPersistenceAdaptater(carRepository, carMapper);
     }
 
     @Bean
-    public CarPortIn getCarPortIn(){
+    public CarPortIn getCarPortIn() {
         carPersistenceAdaptater = new CarPersistenceAdaptater(carRepository, carMapper);
         return new CarListUseCase(carPersistenceAdaptater);
+    }
+
+    @Bean
+    public MotoPortOut getMotoPortOut() {
+        return new MotoPersistenceAdaptater(motoRepository, motoMapper);
+    }
+
+    @Bean
+    public MotoPortIn getMotoPortIn() {
+        motoPersistenceAdaptater = new MotoPersistenceAdaptater(motoRepository, motoMapper);
+        return new MotoListUseCase(motoPersistenceAdaptater);
+
     }
 }
