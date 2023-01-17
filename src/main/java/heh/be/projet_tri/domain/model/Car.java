d5b7a971a0f0f823f39ac2ef33dd700beb51a250
traczi <matteo.alcantarini@hotmail.com>
2023-01-17 15:54:37 +0100

    ajout du model

--- src/main/java/heh/be/projet_tri/domain/model/Car.java
+++ src/main/java/heh/be/projet_tri/domain/model/Car.java
@@ -1,6 +1,25 @@
 package heh.be.projet_tri.domain.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class Car {
+    @Getter
+    private final Long id;
 
+    @Getter
+    private final String marque;
+
+    @Getter
+    private final String model;
+
+    @Getter
+    private final Integer annee;
+
+    @Getter
+    private final Integer prix;
+
+    @Getter
+    private final String image;
 }
