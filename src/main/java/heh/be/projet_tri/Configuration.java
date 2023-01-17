753822d33f3ca0b2f51c3b7bd84526484af9487d
traczi <matteo.alcantarini@hotmail.com>
2023-01-17 16:30:35 +0100

    Ajout de la configuration et du mapper

--- src/main/java/heh/be/projet_tri/Configuration.java
+++ src/main/java/heh/be/projet_tri/Configuration.java
@@ -1,8 +1,32 @@
 package heh.be.projet_tri;
 
+import heh.be.projet_tri.adaptater.out.CarMapper;
+import heh.be.projet_tri.adaptater.out.CarPersistenceAdaptater;
 import heh.be.projet_tri.adaptater.out.CarRepository;
+import heh.be.projet_tri.domain.port.in.CarPortIn;
+import heh.be.projet_tri.domain.port.out.CarPortOut;
+import heh.be.projet_tri.domain.service.CarListUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@org.springframework.context.annotation.Configuration
+@EnableJpaRepositories
 public class Configuration {
+    @Autowired
+    private CarRepository carRepository;
 
+    private CarMapper carMapper = new CarMapper();
+    private CarPersistenceAdaptater carPersistenceAdaptater;
+
+    @Bean
+    public CarPortOut getCarPortOut(){
+        return new CarPersistenceAdaptater(carRepository,carMapper);
+    }
+
+    @Bean
+    public CarPortIn getCarPortIn(){
+        carPersistenceAdaptater = new CarPersistenceAdaptater(carRepository, carMapper);
+        return new CarListUseCase(carPersistenceAdaptater);
+    }
 }
--- src/main/java/heh/be/projet_tri/adaptater/out/CarMapper.java
+++ src/main/java/heh/be/projet_tri/adaptater/out/CarMapper.java
@@ -1,7 +1,26 @@
 package heh.be.projet_tri.adaptater.out;
 
 
+import heh.be.projet_tri.domain.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarMapper {
 
+    List<Car> CarMapJpaToDomain(List<CarJpaEntity> cars){
+        List<Car> carsList = new ArrayList<>();
+
+        for (CarJpaEntity carJpaEntity: cars) {
+            carsList.add(new Car(carJpaEntity.getIdCar(), carJpaEntity.getMarque(), carJpaEntity.getModel(), carJpaEntity.getAnnee(), carJpaEntity.getPrix(), carJpaEntity.getImage()));
+        }
+        return carsList;
+    }
+
+    public CarJpaEntity CarMapDomainToJpa(Car car){
+        return new CarJpaEntity(car.getId(), car.getMarque(), car.getModel(), car.getAnnee(), car.getPrix(), car.getImage());
+    }
+    public Car CarMapJpaToDomain(CarJpaEntity carJpaEntity){
+        return new Car(carJpaEntity.getIdCar(), carJpaEntity.getMarque(), carJpaEntity.getModel(), carJpaEntity.getAnnee(), carJpaEntity.getPrix(), carJpaEntity.getImage());
+    }
 }
